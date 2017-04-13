1b427dfd96511a8a6a68e096c2e335eb6b007ae4
Piet Geursen <pietgeursen@gmail.com>
2017-04-13 15:27:07 +1200

    Adds test for default state

--- test/reducer.js
+++ test/reducer.js
@@ -1,13 +1,28 @@
 'use strict'
 
 const test = require('tape')
+const deepFreeze = require('deep-freeze')
 
-const things = {
+const feathersAction = require('../src/')
+const cats = feathersAction('cats')
+
+const catsRecords = {
   0: { id: 0, name: 'honey', description: 'sweet and delicious.' },
   1: { id: 1, name: 'tea' },
   2: { id: 2, name: 'mug' }
 }
-const thing = things[0]
+
+test('updater returns correct default state', function(t) {
+  state = cats.updater({type: 'woof'})() 
+  expectedState = {
+    cats: {},
+    feathersAction: {
+      requests: {} 
+    }
+  }
+  t.deepEqual(state, expectedState)
+  t.end()
+})
 
 test('find', function (t) {
 })
@@ -25,4 +40,15 @@ test('patch', function (t) {
 })
 
 test('remove', function (t) {
+  const actions = cats.actions 
+
+  let state = { cats: { [thing.id]: thing } }
+  const cid = '1234'
+  const startAction = actions.remove.start(cid, { id: thing.id })
+  state = reducer(state, startAction)
+  t.deepEqual(state.records, {})
+  const successAction = actions.remove.success(cid, thing)
+  state = reducer(state, successAction)
+  t.deepEqual(state.records, {})
+  t.end()
 })
