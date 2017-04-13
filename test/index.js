49598eea49654f0231aabe85b08684df824aa7fd
Piet Geursen <pietgeursen@gmail.com>
2017-04-13 14:39:45 +1200

    Adds test to throw when no args

--- test/index.js
+++ test/index.js
@@ -5,6 +5,11 @@ const feathersAction = require('../src/')
 
 const apiKeyNames = ['actions, updater, epic']
 
+test('feathersAction is a function', function(t) {
+  t.equal(typeof featherAction, 'function')
+  t.end()
+})
+
 test('feathersAction called with a string returns object with action, updater and epic keys', function(t) {
   const modules = feathersAction('cats')
   t.deepEqual(Object.keys(modules), apiKeyNames)
@@ -26,5 +31,10 @@ test('feathersAction called with an array of strings returns object with action,
 test('feathersAction called with an array of objects returns object with action, updater and epic keys', function(t) {
   const modules = feathersAction({name:'cats'}, {name:'dogs'})
   t.deepEqual(Object.keys(modules), apiKeyNames)
+  t.end()
+})
+
+test('throws if called with no args', function(t) {
+  t.throws(() => feathersAction())
   t.end()
 })
