371ea660f124c9af5a96e496dfdfdfd93d67ba2e
Piet Geursen <pietgeursen@gmail.com>
2017-04-13 15:09:38 +1200

    Adds tests for actions

--- test/actions.js
+++ test/actions.js
@@ -1,10 +1,90 @@
 'use strict'
 
 const test = require('tape')
-const createCid = require('cuid')
 
-const createActions = require('../src/actions')
-const FEATHERS_ACTION = require('../src/constants').FEATHERS_ACTION
+const feathersAction = require('../src/')
 
-test('creates actions', function (t) {
+const cats = feathersAction('cats')
+
+test('action creators have correct keys', function (t) {
+  const keys = ['create, update, patch, remove, start, complete, error']
+  t.deepEqual(Object.keys(cats.actions), keys)
+  t.end()
+})
+
+test('create returns the correct action', function(t) {
+  const createAction = cats.actions.create({name: 'fluffy'})
+  const expectedAction = {
+    type: 'FEATHERS_ACTION',
+    payload: {
+      serviceName: 'cats',
+      method: 'create',
+      args: {
+        data: {
+          name: 'fluffy'
+        },
+        query: {}
+      }
+    }
+  } 
+  t.deepEqual(createAction, expectedAction)
+  t.end()
+})
+
+test('find returns the correct action', function(t) {
+  const createAction = cats.actions.create({name: 'fluffy'})
+  const expectedAction = {
+    type: 'FEATHERS_ACTION',
+    payload: {
+      serviceName: 'cats',
+      method: 'find',
+      args: {
+        data: {
+          name: 'fluffy'
+        },
+        query: {}
+      }
+    }
+  } 
+  t.deepEqual(createAction, expectedAction)
+  t.end()
+})
+
+test('patch returns the correct action', function(t) {
+  const createAction = cats.actions.create({id: 1, name: 'fluffy'})
+  const expectedAction = {
+    type: 'FEATHERS_ACTION',
+    payload: {
+      serviceName: 'cats',
+      method: 'patch',
+      args: {
+        data: {
+          id: 1,
+          name: 'fluffy'
+        },
+        query: {}
+      }
+    }
+  } 
+  t.deepEqual(createAction, expectedAction)
+  t.end()
+})
+
+test('remove returns the correct action', function(t) {
+  const createAction = cats.actions.create({id: 1})
+  const expectedAction = {
+    type: 'FEATHERS_ACTION',
+    payload: {
+      serviceName: 'cats',
+      method: 'remove',
+      args: {
+        data: {
+          id: 1
+        },
+        query: {}
+      }
+    }
+  } 
+  t.deepEqual(createAction, expectedAction)
+  t.end()
 })
